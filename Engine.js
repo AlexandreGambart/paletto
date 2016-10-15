b1bb42c7694c6c48cb2de33d637a00bcd2b5fa96
Alexandre Gambart <alexandre1407@hotmail.fr>
2016-10-15 21:13:31 +0200

    Add files via upload

---
+++ Engine.js
@@ -0,0 +1,128 @@
+/**
+ * Created by Alexandre on 15/10/2016.
+ */
+
+
+Pentago = {};
+
+Pentago.Engine = function () {
+    var _board = new Array(4);
+    var _currentPlayer = 'w';
+    var _currentMarbles = 0;
+
+    for (var i = 0; i < 4; i++) {
+        _board[i] = new Array(9);
+    }
+
+    this.getMarbles = function() {
+        return _currentMarbles;
+    };
+
+    this.getSlot = function(subBoard, position) {
+        return _board[subBoard][position];
+    };
+
+    this.getCurrentPlayer = function() {
+        return _currentPlayer;
+    };
+
+    this.addMarble = function(subBoard, position) {
+        if (_board[subBoard][position] !== '') {
+            throw new slotAlreadyOccupiedException();
+        }
+
+        _board[subBoard][position] = _currentPlayer;
+        ++_currentMarbles;
+    };
+
+    this.rotateSubBoard = function(n) {
+        var m0 = _board[n][0];
+        var m1 = _board[n][1];
+        var m2 = _board[n][2];
+        var m3 = _board[n][3];
+        var m5 = _board[n][5];
+        var m6 = _board[n][6];
+        var m7 = _board[n][7];
+        var m8 = _board[n][8];
+
+        _board[n][0] = m6;
+        _board[n][1] = m3;
+        _board[n][2] = m0;
+        _board[n][3] = m7;
+        _board[n][5] = m1;
+        _board[n][6] = m8;
+        _board[n][7] = m5;
+        _board[n][8] = m2;
+    };
+
+    this.reverseRotateSubBoard = function(n) {
+        var m0 = _board[n][0];
+        var m1 = _board[n][1];
+        var m2 = _board[n][2];
+        var m3 = _board[n][3];
+        var m5 = _board[n][5];
+        var m6 = _board[n][6];
+        var m7 = _board[n][7];
+        var m8 = _board[n][8];
+
+        _board[n][0] = m2;
+        _board[n][1] = m5;
+        _board[n][2] = m8;
+        _board[n][3] = m1;
+        _board[n][5] = m7;
+        _board[n][6] = m0;
+        _board[n][7] = m3;
+        _board[n][8] = m6;
+    };
+
+    this.startGame = function() {
+        _currentPlayer = 'w';
+        _currentMarbles = 0;
+        resetBoard();
+        return _currentPlayer;
+    };
+
+    this.switchPlayer = function() {
+        _currentPlayer = _currentPlayer === 'w' ? 'b' : 'w';
+    };
+
+    this.checkWin = function() {
+        // (a1, b1, c1, d1 et e1)
+        if(this.getSlot(0, 0) === this.getSlot(0, 1) &&
+            this.getSlot(0, 1) === this.getSlot(0, 2) &&
+            this.getSlot(0, 2) === this.getSlot(1, 0) &&
+            this.getSlot(1, 0) === this.getSlot(1, 1)) {
+            return true;
+        }
+
+        return false;
+    };
+
+    this.play = function(str) {
+        // c4cbl ;d4abr ;c3ctl ;c3ctl ;c4cbl ;e5cbr ;b1ctl ;b2ctr ;c4cbl ;c3
+        var res = str.split(';');
+        for (var i = 0; i < res.length; i++) {
+            var column = res[i][0].charCodeAt(0) - 97;
+            var row = res[i][1];
+            var rotType = res[i][2];
+            var subBoard = res[i][3] + res[i][4];
+            console.log(column + " " + row + " " + rotType + " " + subBoard);
+        }
+    };
+
+    // var ch = coordonne.charCodeAt(0) - 97;
+    // var ligne = coordonne[1] - 1;
+
+
+    var resetBoard = function() {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 9; j++) {
+                _board[i][j] = '';
+            }
+        }
+    };
+
+    function slotAlreadyOccupiedException() {
+        this.name = "slotAlreadyOccupiedException";
+    }
+};
---
+++ EngineTest.js
@@ -0,0 +1,158 @@
+/**
+ * Created by Alexandre on 15/10/2016.
+ */
+
+
+PentagoTest = TestCase("PentagoTest");
+
+PentagoTest.prototype.testStartGame = function() {
+    var engine = new Pentago.Engine();
+    assertEquals('w', engine.startGame());
+};
+
+PentagoTest.prototype.testGetMarbles = function() {
+    var engine = new Pentago.Engine();
+    assertEquals(0, engine.getMarbles());
+};
+
+PentagoTest.prototype.testGetSlot = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    assertEquals('', engine.getSlot(0, 0));
+    engine.addMarble(0, 0);
+    assertEquals('w', engine.getSlot(0, 0));
+};
+
+PentagoTest.prototype.testAddMarble = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.addMarble(0, 0);
+    assertEquals(1, engine.getMarbles());
+    engine.switchPlayer();
+    engine.addMarble(0, 2);
+    assertEquals(2, engine.getMarbles());
+    assertException(function () { engine.addMarble(0, 0); }, "slotAlreadyOccupiedException");
+};
+
+PentagoTest.prototype.testRotateSubBoard = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.addMarble(0, 0);
+    engine.rotateSubBoard(0);
+    assertEquals('w', engine.getSlot(0, 2));
+};
+
+PentagoTest.prototype.testGetCurrentPlayer = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    assertEquals('w', engine.getCurrentPlayer());
+};
+
+PentagoTest.prototype.testSwitchPlayer = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.switchPlayer();
+    assertEquals('b', engine.getCurrentPlayer());
+};
+
+PentagoTest.prototype.testReverseRotateSubBoard = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.addMarble(0, 0);
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+    engine.addMarble(0, 0);
+    engine.reverseRotateSubBoard(0);
+    assertEquals('w', engine.getSlot(0, 0));
+    assertEquals('b', engine.getSlot(0, 6));
+};
+
+PentagoTest.prototype.testTenthHistory = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.addMarble(0, 0); // a1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 0);
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 1); // b1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 3); // a2
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 2); // c1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 6); // a3
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(1, 0); // d1
+    engine.reverseRotateSubBoard(1);
+    engine.switchPlayer();
+
+    engine.addMarble(1, 8); // f3
+    engine.rotateSubBoard(1);
+
+    assertEquals(8, engine.getMarbles());
+    assertEquals('w', engine.getSlot(0, 0));
+    assertEquals('w', engine.getSlot(0, 1));
+    assertEquals('w', engine.getSlot(0, 2));
+    assertEquals('w', engine.getSlot(1, 0));
+    assertEquals('b', engine.getSlot(0, 6));
+    assertEquals('b', engine.getSlot(0, 7));
+    assertEquals('b', engine.getSlot(0, 8));
+    assertEquals('b', engine.getSlot(1, 6));
+};
+
+PentagoTest.prototype.testPlay = function() {
+    var engine = new Pentago.Engine();
+    engine.play("c4cbl ;d4abr ;c3ctl ;c3ctl ;c4cbl ;e5cbr ;b1ctl ;b2ctr ;c4cbl ;c3");
+};
+
+PentagoTest.prototype.testEleventhHistory = function() {
+    var engine = new Pentago.Engine();
+    engine.startGame();
+    engine.addMarble(0, 0); // a1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 0);
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 1); // b1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 3); // a2
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 2); // c1
+    engine.rotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(0, 6); // a3
+    engine.reverseRotateSubBoard(0);
+    engine.switchPlayer();
+
+    engine.addMarble(1, 0); // d1
+    engine.reverseRotateSubBoard(1);
+    engine.switchPlayer();
+
+    engine.addMarble(1, 8); // f3
+    engine.rotateSubBoard(1);
+    engine.switchPlayer();
+
+    engine.addMarble(1, 1); // e1
+
+    assertEquals(true, engine.checkWin());
+};
